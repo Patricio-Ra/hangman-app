042e02bc83d6a9b7a5c24691ef6ff5a7ff63df64
Patricio R <patricio.raschetti@gmail.com>
2019-12-02 22:49:27 -0300

    Add http req (1)

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,8 @@ const puzzleEl = document.createElement('h1');
 const guessesEl = document.createElement('h4');
 const instructEl = document.createElement('p');
 
+game1.renderGame();
+
 // Events //
 window.addEventListener('keydown', e => {
     const guess = e.key;
@@ -18,4 +20,14 @@ window.addEventListener('keydown', e => {
     }; 
 });
 
-game1.renderGame();
+// Setting up Http request.
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener("readystatechange", e => {
+    if (e.target.readyState === 4 && e.target.status === 200){
+        const data = JSON.parse(xhr.response);
+        console.log(data);
+    }
+});
+xhr.open("GET", "http://puzzle.mead.io/puzzle");
+xhr.send();
