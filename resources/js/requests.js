4cfa1657a64b21191b9301fb1264063e3088110c
Patricio R <patricio.raschetti@gmail.com>
2019-12-05 19:30:16 -0300

    async/await 1

--- resources/js/requests.js
+++ resources/js/requests.js
@@ -1,16 +1,14 @@
 'use strict'
 
 // Puzzle word Http request.
-const getPuzzle = wCount => {
-    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wCount}`, {}).then(response => {
-        if (response.status === 200) {
-            return response.json();
-        } else {
-            throw new Error('Unable to fetch the puzzle.');
-        };
-    }).then(data => {
+const getPuzzle = async wCount => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wCount}`, {})
+    if (response.status === 200) {
+        const data = await response.json();
         return data.puzzle;
-    });
+    } else {
+        throw new Error('Unable to fetch the puzzle.');
+    };
 };
 
 // Country name Http request
