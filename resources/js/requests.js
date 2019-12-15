f9c1320a532325698e16c46bf680e152765a76da
Patricio R <patricio.raschetti@gmail.com>
2019-12-15 20:16:03 -0300

    asd

--- resources/js/requests.js
+++ resources/js/requests.js
@@ -2,7 +2,7 @@
 
 // Puzzle word Http request.
 const getPuzzle = async wCount => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wCount}`, {})
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wCount}`, {});
     if (response.status === 200) {
         const data = await response.json();
         return data.puzzle;
