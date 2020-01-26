7cca9dc1dd90af2bac3221e1a0895a81add4918e
Patricio R <patricio.raschetti@gmail.com>
2020-01-26 00:42:49 -0300

    Requests have no used functions (old)

--- src/requests.js
+++ src/requests.js
@@ -11,31 +11,4 @@ const getPuzzle = async wCount => {
     };
 };
 
-// Country name Http request
-const getCountry = async countryCode => {
-    const response = await fetch('//restcountries.eu/rest/v2/all', {});
-    if (response.status === 200) {
-        const data = await response.json();
-        return data.find(country => country.alpha2Code === countryCode);
-    } else {
-        throw new Error('Unable to fetch country.');
-    };
-};
-
-// Current Country
-const getCurrentCountry = async () => {
-    const location = await getLocation();
-    return getCountry(location.country);
-}
-
-// Location Http request
-const getLocation = async () => {
-    const response = await fetch('//ipinfo.io/json?token=7c7aa52d3e9264', {});
-    if (response.status === 200) {
-        return response.json();
-    } else {
-        throw new Error('Unable to fetch the location.');
-    };
-};
-
 export { getPuzzle as default };
